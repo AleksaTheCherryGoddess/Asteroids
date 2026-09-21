430d87c26bebee82eed3c0e55045bfe04628622b
AleksaTheCherryGoddess <aleksa.deszcz@gmail.com>
2026-09-21 23:03:24 +0200

    add game clock

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from logger import log_state
 def main():
     pygame.init()
 
+    clock=pygame.time.Clock()
+    dt=0.0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
     while True:
@@ -21,6 +24,7 @@ def main():
 
 
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
