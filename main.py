79ca6f48c5994afe28183f7c6aae452d9781d2b7
AleksaTheCherryGoddess <aleksa.deszcz@gmail.com>
2026-09-22 18:11:26 +0200

    add player object with sprite and hitbox

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -12,6 +13,8 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
+    player=Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
     while True:
         log_state()
 
@@ -20,6 +23,8 @@ def main():
                 return
 
         screen.fill('black')
+
+        player.draw(screen)
 
 
 
